1134d61d2e472275e7a6099da64462961572ee58
Daniel Joy <danieljoy2345@gmail.com>
2025-10-23 10:43:10 -0400

    Updating prompting logic to account for different models

--- gemini_guess_location.py
+++ gemini_guess_location.py
@@ -14,6 +14,20 @@ Provide as much information behind your thought process as possible.
 Make the last line of your response be the specific location"""
 API_KEY = ""
 
+def save_results(responses1):
+    """Function to save results in a labelled dataframe, 
+    prevents duplicating code for each except branch"""
+    df = pd.DataFrame(responses1)
+    if model.model_name=="models/gemini-2.5-pro":
+        df.to_csv("results_25pro.csv", index=False)
+    elif model.model_name=="models/gemini-2.5-flash":
+        df.to_csv("results_25flash.csv", index=False)
+    elif model.model_name=="models/gemini-2.0-flash":
+        df.to_csv("results_20flash.csv", index=False)
+    else:
+        df.to_csv("resultsOTHERMODEL.csv", index=False)
+    print("Saved results")
+
 #Configure model parameters to make it slightly random
 generation_config = {
   "temperature": 0.2,
@@ -25,12 +39,11 @@ generation_config = {
 #Pass in api key
 genai.configure(api_key=API_KEY)
 
-#Create model. We're using gemini 2.5 pro, if that his limit, can use 2.5 flash
+#Create model. We're using gemini 2.5 pro, if that hits limit, can use 2.5 flash
 model = genai.GenerativeModel(
-    model_name="gemini-2.5-pro",
+    model_name="gemini-2.5-flash",
     generation_config=generation_config,
 )
-
 #Set up array to collect responses
 responses = []
 
@@ -45,10 +58,26 @@ try:
         responses.append({
             "image": filename,
             "response": response.text})
-        time.sleep(5) #Prevents hitting the 15/min rate limit
-    resultsdf = pd.DataFrame(responses)
-    resultsdf.to_csv("results_25pro.csv", index=False)
+        if model.model_name=="models/gemini-2.5-pro":
+            print("pro 2.5 - sleeping 20 seconds")
+            time.sleep(20) #Prevents hitting the 2/min rate limit
+        elif model.model_name=="models/gemini-2.0-flash":
+            print("flash 2.0 - sleeping 4.5 seconds")
+            time.sleep(4.5) #Prevents hitting the 15/min rate limit
+        else:
+            print("unknown model - sleeping 15 seconds")
+            time.sleep(15)
+        #Flash 2.5 is so slow we don't need to sleep anything
+        #elif model.model_name=="models/gemini-2.5-flash":
+        #    print("flash 2.5 - sleeping 1 second")
+        #    time.sleep(1) #Prevents hitting 10/min rate limit
+    save_results(responses)
 except exceptions.ResourceExhausted: #If we get rate-limited, save results so far
     print("Daily rate limit hit. Saving results so far...")
-    resultsdf = pd.DataFrame(responses)
-    resultsdf.to_csv("results_25pro.csv", index=False)
+    save_results(responses)
+except exceptions.InvalidArgument:
+    print("Invalid API key")
+except KeyboardInterrupt:
+    save_results(responses)
+    print("User stopped program")
+save_results(responses)
